fc8927ef955b9723d3a6c3511010119464cd6fab
Guilherme Aranha <guilherme.aranha.ga@gmail.com>
2022-12-09 18:29:53 -0300

    feat: create process function

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,22 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    file = txt_importer(path_file)
+
+    for i in instance.data:
+        if i['nome_do_arquivo'] == path_file:
+            return None
+
+    result = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file),
+        "linhas_do_arquivo": file,
+    }
+    instance.enqueue(result)
+    sys.stdout.write(str(result))
+    return result
 
 
 def remove(instance):
