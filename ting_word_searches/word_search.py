4820817f6f8f739a4f464fc2b2858c7bb7ec5f50
Guilherme Aranha <guilherme.aranha.ga@gmail.com>
2022-12-09 18:47:49 -0300

    feat: create exists_word function

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,18 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    data = instance.data
+    result = []
+    occurences = []
+    for i in data:
+        for index, line in enumerate(i['linhas_do_arquivo']):
+            if word.lower() in line.lower():
+                occurences.append({'linha': index + 1})
+        if len(occurences) > 0:
+            result.append({
+                'palavra': word,
+                'arquivo': i['nome_do_arquivo'],
+                'ocorrencias': occurences
+            })
+    return result
 
 
 def search_by_word(word, instance):
